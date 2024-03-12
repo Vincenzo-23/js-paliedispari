bf3e2aa75747a58304f297fcec0944d14d8c56ad
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-12 14:27:02 +0100

    ADD: added the js to check if the word written by the user is palindrome or not

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
-console.log("inizio")
-
 //chiediamo all'utente di inserire una parola
+const word = prompt("inserisci una parola")
 
-//creare una funzione per capire se la parola inserita dall'utente è palindroma
-    //la funziona ci restituirà true se la parola è palindroma o false se non lo è
 
-//stampare in console un messaggio per comunicare all'utente il risultato del controllo    
+//creare una funzione per capire se la parola inserita dall'utente è palindroma  
+
+function isPalindrome (word){
+
+    //dichiariamo una variabile alla quale assegnamo come valore la lunghezza della parola inserita dall'utente
+    const wordLength = word.length;
+
+    //creiamo un ciclo per iterare solamente metà della stringa rappresentata dalla parola inserita dall'utente
+    for (let i = 0; i < wordLength / 2; i++){
+ 
+        //SE una lettera della prima metà della parola è uguale alla sua corrispondente nella seconda metà
+        if (word[i] === word[wordLength - 1 - i]){
+            //la funziona ci restituirà true 
+            return "The word is palindrome"
+        }else{
+            //altrimenti ci restituirà false
+            return "The word is not palindrome"
+        }
+
+    }
+}
+
+//dichiaro una variabile dove le assegno la funzione appena creata
+const checkPalindrome = isPalindrome(word)
+
+//stampo in console un messaggio per comunicare all'utente il risultato del controllo    
+console.log(checkPalindrome)
