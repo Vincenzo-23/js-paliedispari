0f86785308ca2c573a7a2c8c95e94af0431e2651
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-12 14:44:45 +0100

    ADD: added 2 folders with inside the html and js files. Added the pseudo code in the js of the even and odd numbers exercise

---
+++ Pari o Dispari/js/app.js
@@ -0,0 +1,15 @@
+console.log("Inizio")
+
+
+//fare scegliere all'utente tra pari e dispari
+
+//l'utente inserisce un numero da 1 a 5
+
+//generiamo un numero random (sempre da 1 a 5) usando una funziona che può essere la math.random
+
+//sommimao i 2 numeri (quello inserito dall'utente e quello generato randomicamente)
+
+//stabiliamo se la somma dei 2 numeri è pari o dispari usando una funzione (che potremmo chiamara isNumberEven)
+
+//SE l'utente indovina che il numero è pari o dispari, avrà vinto
+//ALTRIMENTI avrà vinto il computer
