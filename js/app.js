117d72621d215966345ea5593ad72efe0ea72d3d
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-12 13:49:57 +0100

    ADD: added the html and js file

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log("inizio")
+
+//chiediamo all'utente di inserire una parola
+
+//creare una funzione per capire se la parola inserita dall'utente è palindroma
+    //la funziona ci restituirà true se la parola è palindroma o false se non lo è
+
+//stampare in console un messaggio per comunicare all'utente il risultato del controllo    
