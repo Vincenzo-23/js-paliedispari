0742c0c530b3967c9b07f6dc111fcba060d45ba2
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-12 16:19:45 +0100

    REFACTORING: changed html elements for picking even or odd. ADD: added the js element inside the event listener to generate the game

--- Pari o Dispari/js/app.js
+++ Pari o Dispari/js/app.js
@@ -1,11 +1,3 @@
-//fare scegliere all'utente tra pari e dispari
-    //recupero dal DOM i value degli input pari e dispari con getElementById
-
-const even = document.getElementById("even").value
-
-const odd = document.getElementById("odd").value
-
-
 //recupero dal DOM il pulsante invia al quale poi aggiungo l'event listener "click" per prendere i value precedentemente recuperati dal DOM
 
 const sendButton = document.getElementById("button")
@@ -13,21 +5,54 @@ const sendButton = document.getElementById("button")
 //resto in ascolto del click sul bottone sendButton
 
 sendButton.addEventListener("click", function(){
+
+    //fare scegliere all'utente tra pari e dispari
+    //recupero dal DOM la value dell'input text con la scelta dell'utente tra pari e dispari
+    const evenOdd = document.getElementById("even_odd").value
+    console.log("Your choice is " + evenOdd)
+    
     
     //l'utente inserisce un numero da 1 a 5
     //recupero dal DOM il value dell'input per la scelta del numero con getElementById
-    const userNumber = document.getElementById("number").value
+    const userNumber = parseInt(document.getElementById("number").value)
+    console.log("Your number is " + userNumber)
     
 
 
     //generiamo un numero random (sempre da 1 a 5) usando una funziona che può essere la math.random
+    const cpuNumber = Math.floor(Math.random() * 5) + 1;
+    console.log("CPU number is " + cpuNumber)
+    
     
     //sommiamo i 2 numeri (quello inserito dall'utente e quello generato randomicamente)
+    const numbersSum = userNumber + cpuNumber
+    console.log("The sum is " + numbersSum)
     
+
     //stabiliamo se la somma dei 2 numeri è pari o dispari usando una funzione (che potremmo chiamara isNumberEven)
-    
+    function isnumbersSumEven (number){
+
+        const rest = number % 2;
+
+        if(rest === 0){
+            return "even";
+        }else{
+            return "odd";
+        } 
+    }
+
+    const result = isnumbersSumEven(numbersSum)
+    console.log("The sum is " + result)
+
+
     //SE l'utente indovina che il numero è pari o dispari, avrà vinto
-    //ALTRIMENTI avrà vinto il computer
+    if(evenOdd === result){
+        console.log("You win!")
+
+        //ALTRIMENTI avrà vinto il computer
+    }else{
+        console.log("You lose!")
+    }
 
 
 
